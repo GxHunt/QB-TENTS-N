9ca585c95286d1db034ff0f84f73f8695d9db43d
GxHunt <118712455+GxHunt@users.noreply.github.com>
2022-12-09 04:42:34 +0000

    New Scirpt

---
+++ [newscript]/ncloading/assets/scripts/config.js
@@ -0,0 +1,30 @@
+Config = {}; // Don't touch
+
+Config.ServerIP = "20.205.7.81:30120";
+
+// Social media buttons on the left side
+Config.Socials = [
+    {name: "discord", label: "Discord", description: "Click! Untuk Join Ke Server Discord Tent's", icon: "assets/media/icons/discord.png", link: "https://discord.gg/pmqpmtdeFf"},
+    {name: "instagram", label: "Instagram", description: "Click! Untuk Follow Instagram", icon: "assets/media/icons/tiktok.png", link: "https://www.instagram.com/ic.gbrandon/"},
+    {name: "tebex", label: "Donations", description: "For donations, feel free to look at the room - #Donations at Discord.", icon: "assets/media/icons/tebex.png", link: "sociabuzz.com/gustiatux"},
+];
+
+Config.HideoverlayKeybind = 112 // JS key code https://keycode.info
+Config.CustomBindText = "F1"; // leave as "" if you don't want the bind text in html to be statically set
+
+// Staff list
+Config.Staff = [
+    {name: "Tuks #1", description: "Owner & Dev", color: "#ff0000", image: "https://cdn.discordapp.com/attachments/1034216825132691466/1044328695281754252/Elegant_Initial_Jewelry_Logo_1.png"},
+    
+
+
+];
+
+// Categories
+Config.Categories = [
+    {label: "Social Media", default: true},
+    {label: "Staff", default: false}
+];
+
+// Music
+Config.Song = "song.mp3";
